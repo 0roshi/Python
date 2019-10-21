f2d9fcaa0886c1ab5b15e161331d716e2a0f6cd9
0roshi <56248705+0roshi@users.noreply.github.com>
2019-10-21 13:31:51 +0200

    Update and rename bissextile (civ).py to Exercice2Bonus.py

--- Exercice2Bonus.py
+++ Exercice2Bonus.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+#
+# Petit bonus: après avoir fait l'exercice sur les années bissextiles, j'ai voulu en faire une version qui donnait toutes 
+#les années bissextiles de -3000 à 2020
+#
 #print("Saisissez l'année, vous saurez si elle est bissextile ou non.")
 #annee = int(input())
 #on vérifie que l'année est divisible par 4 et pas par 100, ou par 400
