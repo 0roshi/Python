ac0b2a01481bc3db130a00f68f0ca2c27ee2b855
0roshi <56248705+0roshi@users.noreply.github.com>
2019-10-21 11:37:59 +0200

    Add files via upload

---
+++ bissextile (civ).py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#print("Saisissez l'année, vous saurez si elle est bissextile ou non.")
+#annee = int(input())
+#on vérifie que l'année est divisible par 4 et pas par 100, ou par 400
+for i in range(5021):
+	if (i-3000) % 4 == 0 and (i-3000) % 100 != 0 or (i-3000) % 400 == 0:
+		o=str(i-3000)
+		print(o + " est bissextile") 
+#essai (raté) de valeur invalide, mais python ne laisse pas cela se faire à cause du int() pour annee
+#elif type(annee) != int :
+#	print("La valeur est invalide.")
+	else:
+		continue
+#	print( "L'année n'est pas bissextile.")
+
